79697254e399204063fc3a062aa86d6feae6678e
arekispiroglu <arekispiroglu@MagiClick.com>
2018-03-26 17:18:59 +0300

    development items added

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
-const commando = require('discord.js-commando');
-const bot = new commando.Client();
+// Bot Settings
+const botsettings = require('./botsettings.json');
+// Original Discord js
+const Discord = require('discord.js');
 
-bot.registry.registerGroup('random', 'Random');
-bot.registry.registerDefaults();
-bot.registry.registerCommmandsIn(__dirname + "/commands");
+const bot = new Discord.Client({disableEveryone: true});
+const prefix = botsettings.prefix;
 
-bot.login('NDI2MTE4ODY0MzcyMjM2Mjg5.DZRhrA.jeCBzJwU22bIcJBGSUe5tE2XJjM');
+// bot.registry
+//     .registerGroup('random', 'Random')
+//     .registerDefaults()
+//     .registerCommandsIn(path.join(__dirname, 'commands'));
+
+bot.on('ready', async() => {
+    console.log(`Bot is ready! *** ${bot.user.username} ***`);
+    try {
+        let link = await bot.generateInvite(["ADMINISTRATOR"]);
+        console.log("* Link Created *",link);
+    } catch(e) {
+        console.log(e.stack);
+    }
+});
+
+bot.on('message', async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === 'dm') return;
+
+    let messageArray = message.content.split(' ');
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(!command.startsWith(prefix)) return;
+
+    if(command === `${prefix}userinfo`) {
+        let embed = new Discord.RichEmbed()
+            .setAuthor(message.author.username)
+            .setDescription("This is the user's info!")
+            .setColor("#FFFFFF")
+            .addField("Full Username", `${message.author.username}#${message.author.discriminator}`)
+            .addField("ID", message.author.id)
+            .addField("Created At", message.author.createdAt);
+        message.channel.sendEmbed(embed);
+
+        return;
+    }
+})
+
+// Bot Login
+bot.login(botsettings.token);
